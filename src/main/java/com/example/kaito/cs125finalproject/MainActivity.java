4e389ab6ce5d1d3d5f1d32005e2e0c789a469865
Kayhan <kayhane2@illinois.edu>
2018-12-02 19:53:29 -0600

    managed to get the ranked Data

--- src/main/java/com/example/kaito/cs125finalproject/MainActivity.java
+++ src/main/java/com/example/kaito/cs125finalproject/MainActivity.java
@@ -12,16 +12,25 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
+
     private static RequestQueue requestQueue;
     private TextView summonerNameView;
+    String id;
+    String summonerName;
+    JSONObject copyOfSummonerData;
+    JSONArray rankedDataArray;
+    String APIKey = "?api_key=RGAPI-23623849-6cf7-4e5f-b206-95ede3eb4c62";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,30 +43,36 @@ public class MainActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("tag1", "before api call");
-                startAPICall(v);
-                Log.d("tag2", "after api call");
+                summonerNameCall(v);
             }
         });
     }
-    public void startAPICall(View v) {
+
+    public interface VolleyCallback {
+        void onSuccessResponse(String result);
+    }
+
+    public void getResponse(int method, String url, JSONArray array, final VolleyCallback callback) {
+
+    }
+
+
+    public void summonerNameCall(View v) {
 
         String SummonerName = searchedSummoner();
-        String APIKey = "?api_key=RGAPI-23623849-6cf7-4e5f-b206-95ede3eb4c62";
         String webAPI = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/";
         String url = webAPI + SummonerName + APIKey;
-        Log.d("tag3", "before request");
-
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject summonerData) {
-                        Log.d("tag10", "onResponse starts");
                         try {
-                            Log.d("tag4", "onResponse went through");
-                            String summonerName = summonerData.getString("name");
+                            copyOfSummonerData = summonerData;
+                            Log.d("Tag", summonerData.toString());
+                            summonerName = summonerData.getString("name");
+                            id = summonerData.getString("id");
+                            summonerRankedCall(id);
                             summonerNameView.setText(summonerName);
-
                         } catch (Exception e) {
                             e.printStackTrace();
 
@@ -67,17 +82,34 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
 
-                        Log.d("Taggers", "error in response");
                     }
                 });
         requestQueue.add(jsonObjectRequest);
-        Log.d("tag5", "end of response");
     }
+
+    public void summonerRankedCall(String id) {
+        String rankedURL = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/";
+        String url2 = rankedURL + id + APIKey;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url2, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("rankedData", response.toString());
+                rankedDataArray = response;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+
     public String searchedSummoner() {
 
         SearchView searchBar = findViewById(R.id.searchView);
         String summonerName  = String.valueOf(searchBar.getQuery());
-
         return summonerName;
     }
 }
